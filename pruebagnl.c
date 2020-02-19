ad628d3553c7ac46e4719afc30212720aef13b18
Adrian Ortega fernandez <aortega-@c1r12s4.42madrid.com>
2020-02-19 16:52:31 +0100

    ya

--- pruebagnl.c
+++ pruebagnl.c
@@ -3,14 +3,22 @@
 
 int		get_next_line(char **line)
 {
-	static char *stc;
+	static char	*stc;
 	char		*tmp;
 	int			len;
-	char		buff[512];
+	char		buff[32];
 
 	if (!line)
 		return (-1);
 	if (stc == NULL)
 		stc = ft_strdup("");
-	
+	while (!(ft_strchr(stc)))
+	{
+		len = read(0, buff, 31);
+		if (len < 0)
+			return (-1);
+		if (!len)
+			break;
+	}
+
 }
