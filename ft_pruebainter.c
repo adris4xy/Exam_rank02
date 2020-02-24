47c1a3ede1a56f0a2bfab4ca37a57e2619dc072d
Adrian Ortega fernandez <aortega-@c1r12s4.42madrid.com>
2020-02-24 19:08:25 +0100

    come onnnnn

--- ft_pruebainter.c
+++ ft_pruebainter.c
@@ -6,13 +6,13 @@
 /*   By: aortega- <aortega-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 16:46:15 by aortega-          #+#    #+#             */
-/*   Updated: 2020/02/18 17:19:09 by aortega-         ###   ########.fr       */
+/*   Updated: 2020/02/19 18:13:38 by aortega-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int		ft_not(char *str, char c, int index)
+int		not_string(char *str, char c, int index)
 {
 	int i = 0;
 	while (i < index)
@@ -28,25 +28,25 @@ void	ft_union(char *s1, char *s2)
 {
 	int i = 0;
 	int len = 0;
-	while (s1[i] != '\0')
+	while (s1[i])
 	{
-		if (ft_not(s1, s1[i], i))
+		if (not_string(s1, s1[i], i))
 			write(1, &s1[i], 1);
 		i++;
 	}
 	while (s2[len])
 	{
-		if (ft_not(s2, s2[len], len))
-			if (ft_not(s1, s2[len], i))
+		if (not_string(s2, s2[len], len))
+			if (not_string(s1, s2[len], i))
 				write(1, &s2[len], 1);
 		len++;
 	}
-}
+}	
 
-int	main(int argc, char **argv)
+int  main(int argc, char **argv)
 {
 	if (argc == 3)
 		ft_union(argv[1], argv[2]);
 	write(1, "\n", 1);
-	return(0);	
+	return (0); 
 }
--- pruebagnl.c
+++ pruebagnl.c
@@ -1,12 +1,68 @@
 
 #include "get_next_line.h"
 
+int ft_strlen(char *s)
+{
+	int i = 0;
+	while(s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+	char *s;
+
+	if (!(s = malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1)))
+		return (NULL);
+	while (s1[i])
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		s[i] = s2[j];
+		i++;
+		j++;
+	}
+	s[i] = 0;
+	return (s);
+}
+
+char	*ft_strdup(char *s)
+{
+	int 	i = 0;
+	char	*str;
+
+	if (!(str = malloc(sizeof(char) * ft_strlen(s) + 1)))
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
+
+char	*ft_strchr(char *s)
+{
+	while (*s && *s != '\n')
+		s++;
+	if (*s == '\n')
+		return (s);
+	return (NULL);
+}
+
 int		get_next_line(char **line)
 {
-	static char	*stc;
-	char		*tmp;
-	int			len;
-	char		buff[32];
+	static char		*stc;
+	char			*tmp;
+	char			buff[32];
+	int				len;
 
 	if (!line)
 		return (-1);
@@ -16,9 +72,28 @@ int		get_next_line(char **line)
 	{
 		len = read(0, buff, 31);
 		if (len < 0)
-			return (-1);
+			return(-1);
 		if (!len)
 			break;
+		buff[len] = 0;
+		tmp = ft_strjoin(stc, buff);
+		free(stc);
+		stc = tmp;
 	}
-
+	if ((tmp = ft_strchr(stc)))
+	{
+		*tmp = 0;
+		*line = ft_strdup(stc);
+		tmp = ft_strdup(++tmp);
+		free(stc);
+		stc = tmp;
+		return(1);
+	}
+	else
+	{
+		*line = ft_strdup(stc);
+		free(stc);
+		stc = NULL;
+		return(0);
+	}
 }
